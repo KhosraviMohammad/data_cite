29dc3fb5a707c0ce388c9a06e8a96c09e9cfdf3a
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-06-21 19:49:24 +0330

    mistake query params are fixed

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from utils import Provider, Client, Doi, slice_list
 
 def get_client_doi_count(*, consortium_id):
     client_and_doi = []
-    client = Client(query_params={'consortium-id': consortium_id})
-    client.get_all_data()
-    cleared_data = client.get_main_data()
-    all_clients = cleared_data.get('all_clients')
-    for client_id in all_clients:
+    provide = Provider(query_params={'consortium-id': consortium_id})
+    provide.get_all_data()
+    provide_client_list = provide.get_clients()
+    for client_id in provide_client_list:
         doi = Doi(query_params={'client_id': client_id})
         doi.get_data(count=1)
         doi_count = doi.meta.get('total')
